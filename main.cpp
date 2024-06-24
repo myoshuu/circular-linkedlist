e66d669365d5570c09376277bda3fdf9c3ea0a28
myoshuu <myoshu.me@gmail.com>
2024-06-24 13:41:38 +0700

    feat: fix data structure

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
-#include <string>
-#include <unistd.h>
+#include <cstring>
+#include <limits>
 
-struct Mahasiswa
+struct PeminjamanRuang
 {
-  int nim;
-  std::string nama;
-  std::string jurusan;
-  Mahasiswa *next;
+  char pinjamID[10];
+  char namaRuang[255];
+  char lokasi[100];
+  char tanggalMulaiPinjam[10];
+  char jamMulaiPinjam[5];
+  char tanggalSelesaiPinjam[10];
+  char jamSelesaiPinjam[5];
+  char namaPeminjam[255];
+  char keperluan[255];
+  PeminjamanRuang *next;
 };
 
-Mahasiswa *head = nullptr;
-Mahasiswa *last = nullptr;
+PeminjamanRuang *head = nullptr;
+PeminjamanRuang *last = nullptr;
 
 void clearScreen()
 {
@@ -22,9 +28,20 @@ void clearScreen()
 #endif
 }
 
-void tambahData(int nim, const std::string &nama, const std::string &jurusan)
+void tambahData(const char *pinjamID, const char *namaRuang, const char *lokasi, const char *tanggalMulaiPinjam, const char *jamMulaiPinjam, const char *tanggalSelesaiPinjam, const char *jamSelesaiPinjam, const char *namaPeminjam, const char *keperluan)
 {
-  Mahasiswa *newNode = new Mahasiswa{nim, nama, jurusan, nullptr};
+  PeminjamanRuang *newNode = new PeminjamanRuang;
+  strcpy(newNode->pinjamID, pinjamID);
+  strcpy(newNode->namaRuang, namaRuang);
+  strcpy(newNode->lokasi, lokasi);
+  strcpy(newNode->tanggalMulaiPinjam, tanggalMulaiPinjam);
+  strcpy(newNode->jamMulaiPinjam, jamMulaiPinjam);
+  strcpy(newNode->tanggalSelesaiPinjam, tanggalSelesaiPinjam);
+  strcpy(newNode->jamSelesaiPinjam, jamSelesaiPinjam);
+  strcpy(newNode->namaPeminjam, namaPeminjam);
+  strcpy(newNode->keperluan, keperluan);
+  newNode->next = nullptr;
+
   if (head == nullptr)
   {
     head = newNode;
@@ -33,21 +50,21 @@ void tambahData(int nim, const std::string &nama, const std::string &jurusan)
   }
   else
   {
-    newNode->next = head;
     last->next = newNode;
+    newNode->next = head;
     last = newNode;
   }
 }
 
-Mahasiswa *cariDataByNim(int nim)
+PeminjamanRuang *cariDataByPinjamID(const char *pinjamID)
 {
   if (head == nullptr)
     return nullptr;
 
-  Mahasiswa *curr = head;
+  PeminjamanRuang *curr = head;
   do
   {
-    if (curr->nim == nim)
+    if (strcmp(curr->pinjamID, pinjamID) == 0)
       return curr;
     curr = curr->next;
   } while (curr != head);
@@ -55,32 +72,37 @@ Mahasiswa *cariDataByNim(int nim)
   return nullptr;
 }
 
-void ubahData(int nim, const std::string &nama, const std::string &jurusan)
+void ubahData(const char *pinjamID, const char *namaRuang, const char *lokasi, const char *tanggalMulaiPinjam, const char *jamMulaiPinjam, const char *tanggalSelesaiPinjam, const char *jamSelesaiPinjam, const char *namaPeminjam, const char *keperluan)
 {
-  Mahasiswa *mahasiswa = cariDataByNim(nim);
-  if (mahasiswa)
+  PeminjamanRuang *peminjaman = cariDataByPinjamID(pinjamID);
+  if (peminjaman)
   {
-    mahasiswa->nama = nama;
-    mahasiswa->jurusan = jurusan;
+    strcpy(peminjaman->namaRuang, namaRuang);
+    strcpy(peminjaman->lokasi, lokasi);
+    strcpy(peminjaman->tanggalMulaiPinjam, tanggalMulaiPinjam);
+    strcpy(peminjaman->jamMulaiPinjam, jamMulaiPinjam);
+    strcpy(peminjaman->tanggalSelesaiPinjam, tanggalSelesaiPinjam);
+    strcpy(peminjaman->jamSelesaiPinjam, jamSelesaiPinjam);
+    strcpy(peminjaman->namaPeminjam, namaPeminjam);
+    strcpy(peminjaman->keperluan, keperluan);
   }
   else
   {
-    std::cout << "Data dengan NIM " << nim << " tidak ditemukan.\n";
+    std::cout << "Data dengan PinjamID " << pinjamID << " tidak ditemukan.\n";
   }
 }
 
-// Fungsi untuk menghapus data mahasiswa
-void hapusData(int nim)
+void hapusData(const char *pinjamID)
 {
   if (head == nullptr)
     return;
 
-  Mahasiswa *curr = head;
-  Mahasiswa *previous = last;
+  PeminjamanRuang *curr = head;
+  PeminjamanRuang *previous = last;
 
   do
   {
-    if (curr->nim == nim)
+    if (strcmp(curr->pinjamID, pinjamID) == 0)
     {
       if (curr == head && curr == last)
       {
@@ -112,36 +134,49 @@ void hapusData(int nim)
   } while (curr != head);
 }
 
-// Fungsi untuk mencetak semua data mahasiswa
 void cetakData()
 {
   if (head == nullptr)
   {
-    std::cout << "Tidak ada data mahasiswa.\n";
-    sleep(3);
+    std::cout << "Tidak ada data peminjaman ruang.\n";
+    std::cout << "Tekan tombol apapun untuk melanjutkan...";
+    std::cin.ignore();
+    std::cin.get();
     return;
   }
 
-  Mahasiswa *curr = head;
+  PeminjamanRuang *curr = head;
   do
   {
-    std::cout << "NIM: " << curr->nim << ", Nama: " << curr->nama << ", Jurusan: " << curr->jurusan << std::endl;
+    std::cout << "PinjamID: " << curr->pinjamID << ", Nama Ruang: " << curr->namaRuang << ", Lokasi: " << curr->lokasi
+              << ", Tanggal Mulai Pinjam: " << curr->tanggalMulaiPinjam << ", Jam Mulai Pinjam: " << curr->jamMulaiPinjam
+              << ", Tanggal Selesai Pinjam: " << curr->tanggalSelesaiPinjam << ", Jam Selesai Pinjam: " << curr->jamSelesaiPinjam
+              << ", Nama Peminjam: " << curr->namaPeminjam << ", Keperluan: " << curr->keperluan << std::endl;
     curr = curr->next;
-    sleep(2);
   } while (curr != head);
+  std::cout << "Tekan tombol apapun untuk melanjutkan...";
+  std::cin.ignore();
+  std::cin.get();
+}
+
+void tungguInput()
+{
+  std::cout << "Tekan tombol apapun untuk melanjutkan...";
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  std::cin.get();
 }
 
 int main()
 {
-  int pilihan, nim;
-  std::string nama, jurusan;
+  int pilihan;
+  char pinjamID[20], namaRuang[50], lokasi[50], tanggalMulaiPinjam[15], jamMulaiPinjam[10], tanggalSelesaiPinjam[15], jamSelesaiPinjam[10], namaPeminjam[50], keperluan[100];
 
   do
   {
     clearScreen();
     std::cout << "Menu:\n";
     std::cout << "1. Input/tambah data\n";
-    std::cout << "2. Cari data berdasarkan NIM\n";
+    std::cout << "2. Cari data berdasarkan PinjamID\n";
     std::cout << "3. Ubah data\n";
     std::cout << "4. Hapus data\n";
     std::cout << "5. Cetak semua data\n";
@@ -153,46 +188,74 @@ int main()
     {
     case 1:
       clearScreen();
-      std::cout << "Masukkan NIM: ";
-      std::cin >> nim;
-      std::cout << "Masukkan Nama: ";
+      std::cout << "Masukkan PinjamID: ";
+      std::cin >> pinjamID;
+      std::cout << "Masukkan Nama Ruang: ";
       std::cin.ignore();
-      std::getline(std::cin, nama);
-      std::cout << "Masukkan Jurusan: ";
-      std::getline(std::cin, jurusan);
-      tambahData(nim, nama, jurusan);
+      std::cin.getline(namaRuang, 50);
+      std::cout << "Masukkan Lokasi: ";
+      std::cin.getline(lokasi, 50);
+      std::cout << "Masukkan Tanggal Mulai Pinjam (DD-MM-YYYY): ";
+      std::cin.getline(tanggalMulaiPinjam, 15);
+      std::cout << "Masukkan Jam Mulai Pinjam (HH:MM): ";
+      std::cin.getline(jamMulaiPinjam, 10);
+      std::cout << "Masukkan Tanggal Selesai Pinjam (DD-MM-YYYY): ";
+      std::cin.getline(tanggalSelesaiPinjam, 15);
+      std::cout << "Masukkan Jam Selesai Pinjam (HH:MM): ";
+      std::cin.getline(jamSelesaiPinjam, 10);
+      std::cout << "Masukkan Nama Peminjam: ";
+      std::cin.getline(namaPeminjam, 50);
+      std::cout << "Masukkan Keperluan: ";
+      std::cin.getline(keperluan, 100);
+      tambahData(pinjamID, namaRuang, lokasi, tanggalMulaiPinjam, jamMulaiPinjam, tanggalSelesaiPinjam, jamSelesaiPinjam, namaPeminjam, keperluan);
       break;
     case 2:
       clearScreen();
-      std::cout << "Masukkan NIM yang dicari: ";
-      std::cin >> nim;
-      if (Mahasiswa *mhs = cariDataByNim(nim))
+      std::cout << "Masukkan PinjamID yang dicari: ";
+      std::cin >> pinjamID;
+      if (PeminjamanRuang *pr = cariDataByPinjamID(pinjamID))
       {
-        std::cout << "Data ditemukan: NIM: " << mhs->nim << ", Nama: " << mhs->nama << ", Jurusan: " << mhs->jurusan << std::endl;
-        sleep(3);
+        std::cout << "Data ditemukan: PinjamID: " << pr->pinjamID << ", Nama Ruang: " << pr->namaRuang << ", Lokasi: " << pr->lokasi
+                  << ", Tanggal Mulai Pinjam: " << pr->tanggalMulaiPinjam << ", Jam Mulai Pinjam: " << pr->jamMulaiPinjam
+                  << ", Tanggal Selesai Pinjam: " << pr->tanggalSelesaiPinjam << ", Jam Selesai Pinjam: " << pr->jamSelesaiPinjam
+                  << ", Nama Peminjam: " << pr->namaPeminjam << ", Keperluan: " << pr->keperluan << std::endl;
       }
       else
       {
         std::cout << "Data tidak ditemukan.\n";
-        sleep(3);
       }
+      tungguInput();
       break;
     case 3:
       clearScreen();
-      std::cout << "Masukkan NIM yang akan diubah: ";
-      std::cin >> nim;
-      std::cout << "Masukkan Nama baru: ";
+      std::cout << "Masukkan PinjamID yang akan diubah: ";
+      std::cin >> pinjamID;
+      std::cout << "Masukkan Nama Ruang baru: ";
       std::cin.ignore();
-      std::getline(std::cin, nama);
-      std::cout << "Masukkan Jurusan baru: ";
-      std::getline(std::cin, jurusan);
-      ubahData(nim, nama, jurusan);
+      std::cin.getline(namaRuang, 50);
+      std::cout << "Masukkan Lokasi baru: ";
+      std::cin.getline(lokasi, 50);
+      std::cout << "Masukkan Tanggal Mulai Pinjam baru (DD-MM-YYYY): ";
+      std::cin.getline(tanggalMulaiPinjam, 15);
+      std::cout << "Masukkan Jam Mulai Pinjam baru (HH:MM): ";
+      std::cin.getline(jamMulaiPinjam, 10);
+      std::cout << "Masukkan Tanggal Selesai Pinjam baru (DD-MM-YYYY): ";
+      std::cin.getline(tanggalSelesaiPinjam, 15);
+      std::cout << "Masukkan Jam Selesai Pinjam baru (HH:MM): ";
+      std::cin.getline(jamSelesaiPinjam, 10);
+      std::cout << "Masukkan Nama Peminjam baru: ";
+      std::cin.getline(namaPeminjam, 50);
+      std::cout << "Masukkan Keperluan baru: ";
+      std::cin.getline(keperluan, 100);
+      ubahData(pinjamID, namaRuang, lokasi, tanggalMulaiPinjam, jamMulaiPinjam, tanggalSelesaiPinjam, jamSelesaiPinjam, namaPeminjam, keperluan);
+      tungguInput();
       break;
     case 4:
       clearScreen();
-      std::cout << "Masukkan NIM yang akan dihapus: ";
-      std::cin >> nim;
-      hapusData(nim);
+      std::cout << "Masukkan PinjamID yang akan dihapus: ";
+      std::cin >> pinjamID;
+      hapusData(pinjamID);
+      tungguInput();
       break;
     case 5:
       cetakData();
@@ -201,6 +264,7 @@ int main()
       return 0;
     default:
       std::cout << "Pilihan tidak valid.\n";
+      tungguInput();
     }
   } while (pilihan != 6);
 }
